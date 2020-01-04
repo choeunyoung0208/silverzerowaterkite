29f6eff2b89d4a8e75abe0534d992b4392a90e35
choeunyoung0208 <59273572+choeunyoung0208@users.noreply.github.com>
2020-01-04 23:38:59 +0900

    Update Untitled3.m

--- 200104/Untitled3.m
+++ 200104/Untitled3.m
@@ -2,7 +2,7 @@
 
 %다 끝내고 싶었지만....다 끝내지 못해...일단은 여기까지만...
 
-
+f=imread('lena.jpg');
 %[counts,binLocations] = imhist(f) 명령어를 사용해 영상 f의 각 그레이 레벨에 해당되는 빈도수(픽셀수) 확인
 %counts : 히스토그램 도수
 %binLocations : Bin 위치(영상 f의 그레이 레벨 스펙트럼)
